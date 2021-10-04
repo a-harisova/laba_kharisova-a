9e031f94fe9fdb526a8bce4a158a313a45b80332
a-harisova <kharisova-adilya@yandex.ru>
2021-10-04 11:26:20 +0300

    Output in file: PIPE and KS

---
+++ laba_of_ci/laba_of_ci/laba_of_ci.cpp
@@ -0,0 +1,146 @@
+﻿// laba_c++.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+#include <iostream>
+#include <string>
+#include <fstream>
+
+using namespace std;
+
+struct PIPE
+{
+	int id;
+	double length;
+	double diameter;
+	string repair;
+};
+
+struct KS
+{
+	int id;
+	string name;
+	int number_of_workshops;
+	int number_of_working_workshops;
+	double efficiency;
+};
+
+void Output_Pipe(const PIPE& pipe)
+{
+	cout << "PIPE INFORMATION: " << endl;
+	cout << "ID = " << pipe.id << endl;
+	cout << "Length = " << pipe.length << endl;
+	cout << "Diameter = " << pipe.diameter << endl;
+	cout << "Indicator under repair = " << pipe.repair << endl;
+}
+
+void Output_KS(const KS& ks)
+{
+	cout << "KS INFORMATION: " << endl;
+	cout << "ID = " << ks.id << endl;
+	cout << "Name = " << ks.name << endl;
+	cout << "The number of workshops of the compressor station = " << ks.number_of_workshops << endl;
+	cout << "The number of workshops of the compressor station, that are currently operating = " << ks.number_of_working_workshops << endl;
+	cout << "Efficiency, % = " << ks.efficiency << endl;
+}
+
+
+
+PIPE Input_Pipe()
+{
+	PIPE pipe;
+	pipe.id = 0;
+	cout << "How long is the pipe? Please enter a number without spaces: ";
+	cin >> pipe.length;
+	cout << "What is the diameter of the pipe? Please enter a number: ";
+	cin >> pipe.diameter;
+	pipe.repair = "no";
+	return pipe;
+}
+
+KS Input_KS()
+{
+	KS ks;
+	ks.id = 0;
+	cout << "Please enter the name of compressor station: ";
+	cin >> ks.name;
+	cout << "Please enter the number of workshops of the compressor station: ";
+	cin >> ks.number_of_workshops;
+	cout << "Please enter the number of workshops of the compressor station, that are currently operating: ";
+	cin >> ks.number_of_working_workshops;
+	ks.efficiency = 80;
+	return ks;
+}
+
+void Output_In_File_Pipe(const PIPE& pipe)
+{
+	ofstream fout;
+	fout.open("PIPE.txt");
+	if (pipe.length == 0)
+	{
+		if (!fout.is_open())
+		{
+			cout << "Error, text file didn't open." << endl;
+		}
+		else 
+		{
+			fout << "No pipe information, fill in the data an try again." << endl;
+		}
+	}
+	else
+	{
+		fout << "PIPE INFORMATION:" << endl;
+		fout << "ID = " << pipe.id << endl;
+		fout << "Length = " << pipe.length << endl;
+		fout << "Diameter = " << pipe.diameter << endl;
+		fout << "Indicator under repair = " << pipe.repair << endl;
+	}
+	fout.close();
+}
+
+void Output_In_File_KS(const KS& ks)
+{
+	ofstream fout;
+	fout.open("KS.txt");
+	if (ks.number_of_workshops <= 0)
+	{
+		if (!fout.is_open())
+		{
+			cout << "Error, text file didn't open." << endl;
+		}
+		else
+		{
+			fout << "No KS information, fill in the data an try again." << endl;
+		}
+	}
+	else
+	{
+		fout << "KS INFORMATION:" << endl;
+		fout << "ID = " << ks.id << endl;
+		fout << "Nime = " << ks.name << endl;
+		fout << "Number of workshops = " << ks.number_of_workshops << endl;
+		fout << "Number of working workshops = " << ks.number_of_working_workshops << endl;
+		fout << "Efficiency, % = " << ks.efficiency << endl;
+	}
+	fout.close();
+}
+
+int main()
+{
+	PIPE pipe = Input_Pipe();
+	KS ks = Input_KS();
+	Output_Pipe(pipe);
+	Output_KS(ks);
+	Output_In_File_Pipe(pipe);
+	Output_In_File_KS(ks);
+	return 0;
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
